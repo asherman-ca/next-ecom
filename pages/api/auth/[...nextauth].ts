1eb07386125abb104bf00472062972f0ae96aebb
Alex Sherman <asherman.ca@gmail.com>
2023-04-26 16:00:13 -0700

    nextauth type fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
@@ -6,7 +6,7 @@ import Stripe from 'stripe'
 
 const prisma = new PrismaClient()
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
@@ -16,8 +16,7 @@ export const authOptions = {
 		}),
 	],
 	events: {
-		createUser: async ({ user }: any) => {
-			// console.log('createUser', message)
+		createUser: async ({ user }) => {
 			const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 				apiVersion: '2022-11-15',
 			})
